a21ccfce80bc72742587875cda228af2e52dbc11
mariovaliente0 <mariovaliente0@hotmail.com>
2025-03-15 15:29:53 +0100

    Añadir clase Paquete

--- src/paqueteria/Paquete.java
+++ src/paqueteria/Paquete.java
@@ -1,6 +1,9 @@
 package paqueteria;
 
-public class Paquete {
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Paquete implements Comparable<Paquete> {
     public static final int ALTA = 1;
     public static final int MEDIA = 2;
     public static final int BAJA = 3;
@@ -11,7 +14,7 @@ public class Paquete {
         this.producto = pro;
         this.direccionDestino = dd;
         this.prioridad = pri;
-        if (pri<0 || pri>3){
+        if (pri<Paquete.ALTA|| pri>Paquete.BAJA){
             throw new IllegalArgumentException("Nivel de prioridad no válido");
         }
     }
@@ -23,5 +26,23 @@ public class Paquete {
     }
     public int getPrioridad(){
         return prioridad;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Paquete paquete = (Paquete) o;
+        return Objects.equals(producto, paquete.producto) && Objects.equals(direccionDestino, paquete.direccionDestino);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(producto, direccionDestino);
+    }
+
+    @Override
+    public int compareTo(Paquete o) {
+        return this.prioridad - o.prioridad;
     }
 }
